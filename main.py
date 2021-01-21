86575edc9c125fc13a008d96c6afe20a885f35d3
v1enna <19vienna20@gmail.com>
2021-01-21 21:20:56 +0100

    Add assets download except trailers

--- main.py
+++ main.py
@@ -4,29 +4,70 @@
 import PySimpleGUI as gui
 import requests
 import cloudscraper
+import urllib.request
 import io
 
 from pathlib import Path
 from bs4 import BeautifulSoup
 
-def content_parser(url, html, movieID):
-    Path(movieID).mkdir(exist_ok = True)      
-    Path(movieID + "/assets/").mkdir(exist_ok = True)
+def downloadContent(url, movieID):
+    request = cloudscraper.create_scraper().get(url)
+    handler = BeautifulSoup(request.text, features = "html.parser")
 
-    soup = BeautifulSoup(html, features = "html.parser")
-    parent = soup.find("li", { "data-price" : "500" })
-    children = parent.findChildren("a")
+    # header
 
-    with io.open(movieID + "/html.txt", "w+", encoding = "utf-8") as f:
-        f.write(children[0]["href"])
+    content  = '<p style="text-align: center;"><strong><span style="font-size:22px;"><a href="MOVIE LINK" rel="nofollow">MOVIE ID</a></span><br />'
+    content += '<span style="font-size:26px;"><a href="ACTRESS(es) LINK" rel="nofollow">ACTRESS</a>'
+    content += '<span style="font-size:26px;"><a href="MOVIE LINK">&minus;<strong>TITLE&quot;</strong></span><br />'
+    content += '<a href="MOVIE LINK"/><img alt="" src="IMAGE LINK" /></a></p>'
 
+    # table
 
+    content += '<table align="center" border="1" cellpadding="1" cellspacing="1" style="width:400px"><tbody>'
+    content += '<tr><td><strong>Movie</strong></td> <td><a href="MOVIE LINK" rel="nofollow">MOVIE ID</a></td></tr>'
+    content += '<tr><td><strong>Studio</strong></td><td><a href="STUDIO LINK" rel="nofollow">STUDIO NAME</a></td></tr>'
+    content += '<tr><td><strong>Cast</strong></td><td><a href="ACTRESS(es) LINK(s)" rel="nofollow">ACTRESS(es) NAME(s)</a></td></tr>'
+    content += '<tr><td><strong>Release Date</strong></td><td>RELEASE DATE</td></tr></tbody>'
+
+    # content-id
+
+    contentID = handler.find(string = "Content ID:").find_next("dd")
+
+    # download header image
+
+    headerDownload = cloudscraper.create_scraper().get('https://pics.r18.com/digital/video/' + contentID.text.strip() + '/' + contentID.text.strip() + 'pl.jpg', allow_redirects = True)
+    open("requests/" + movieID + "/assets/" + movieID + '-JAV-Actresses-Header.jpg', 'wb').write(headerDownload.content)
+
+    # download images
+
+    for i in range(1, 6, 1):
+        imageDownload = cloudscraper.create_scraper().get('https://pics.r18.com/digital/video/' + contentID.text.strip() + '/' + contentID.text.strip() + 'jp-' + str(i) +'.jpg', allow_redirects = True)
+        print('https://pics.r18.com/digital/video/' + contentID.text.strip() + '/' + contentID.text.strip() + 'jp-' + str(i) +'.jpg')
+        f = open("requests/" + movieID + "/assets/" + movieID + '-JAV-Actresses-0' + str(i) + '.jpg', 'wb')
+        f.write(imageDownload.content)
+        f.close()
+
+    return content
+
+def prepareContent(url, html, movieID):
+    Path("requests/").mkdir(exist_ok = True)
+    Path('requests/' + movieID).mkdir(exist_ok = True)      
+    Path('requests/' + movieID + "/assets/").mkdir(exist_ok = True)
+
+    handler = BeautifulSoup(html, features = "html.parser")
+    parent = handler.find("li", { "data-price" : "500" })
+    url = parent.findChildren("a")
+
+    downloadResult = downloadContent(url[0]["href"], movieID)
+
+    with io.open("requests/" + movieID + "/html.txt", "w+", encoding = "utf-8") as f:
+        f.write(downloadResult)
 
 layout = [
     [
         gui.Text("Movie ID: "),
         gui.In(size = (15, 1), enable_events = True, key = "-ID-"),
-        gui.Button("Search", key = "-SEARCH-")
+        gui.Button("Search", key = "-SEARCH-", bind_return_key = True)
     ],
     [
         gui.Text('_' * 75)
@@ -50,7 +91,7 @@ while True:
 
         if request.text.find("1 titles found") != -1:
             frame.Element("-RESPONSE-").Update("Movie found! Parsing...")
-            content_parser(url, request.text, values["-ID-"].upper())
+            prepareContent(url, request.text, values["-ID-"].upper())
             frame.Element("-RESPONSE-").Update("Request accepted. Files generated in /" + values["-ID-"].upper() + "/.")
         else:
             frame.Element("-RESPONSE-").Update("Movie not found.")
